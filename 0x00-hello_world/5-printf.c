6e211c7541c56baec19ded4c5b0eb0b3bb919f30
FranQie93 <franq.santos.fs@gmail.com>
2023-06-15 13:15:49 -0700

    0-preprocessor

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,19 +1,11 @@
-/*
- * File: 5-printf.c
- * Auth: Francis Yaw Quaye
- */
-
-#include <stdio.h>
-
+#include<stdio.h>
 /**
- * main - Prints "with proper grammar, but the outcome is
- *                a piece of art," followed by a new line.
- *
- * Return: Always 0.
- */
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-
-	return (0);
+	return (0);
 }
